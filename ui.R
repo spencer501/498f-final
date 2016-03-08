a06cc6ea864b98c5c18c2b9965158e7a84add169
Spencer <spencer.p.816@gmail.com>
2016-03-07 18:14:27 -0800

    Begin work on Shiny UI (refrence issue #6 and issue #8).
    TODO:
    - update selectInput based on status of checkbox
    - change "Amount Range" slider to update for each candidate
    
    QUESTIONS:
    - do we want to display data for all candidates at once? If so, where do we put that in the layout
    - do we care about negative expensese?

--- ui.R
+++ ui.R
@@ -1,1 +1,48 @@
-# Required packages
+#########################################
+# `iu.R`
+# Builds the interface for the shiny app
+#########################################
+
+# Needed libraries
+library(shiny)
+
+# Create UI
+shinyUI(fluidPage(
+   # Title of UI
+   titlePanel("2016 Presidential Campaign Spending"),
+   
+   # Create Sidebar Layout
+   sidebarLayout(
+      
+      # Inputs
+      sidebarPanel(
+         # show only currently running candidates
+         checkboxInput("ckbx_running", label = "Display only currently running candidates:", value = TRUE),
+         
+         # choose candidate
+         selectInput("slctbx_candidate", label = "Candidate",
+                     choices = c("a", "b", "c", "d"),
+                     selected = "a"
+                     ),
+         
+         # spending range
+         sliderInput("sldr_amount", label = "Amount Range",
+                     min = -3000.00, 
+                     max = 700000.00,
+                     value = c(-3000.00, 700000.00),
+                     step = 10000)
+         
+         
+      ),
+      
+      # Display outputs
+      mainPanel(
+         tabsetPanel(type = "tabs", 
+                     tabPanel("Plot"), 
+                     tabPanel("Summary"), 
+                     tabPanel("Table")
+         )
+      )
+   )
+   
+))
