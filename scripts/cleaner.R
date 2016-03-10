432f4383eeb9717320fb9627b00ba29cf6c6b501
Spencer <spencer.p.816@gmail.com>
2016-03-10 13:38:28 -0800

    Update `cleaner.R` to no longer be a function (you only clean the data once).

--- scripts/cleaner.R
+++ scripts/cleaner.R
@@ -12,8 +12,13 @@ library(dplyr)
 library(stringr)
 library(tools)
 
+
+##################
+## Initial data ##
+##################
+
 # testing
-#file_name <- 'data/2016_campaign_finances.csv'
+file_name <- 'data/2016_campaign_finances.csv'
 
 # Read in data about the political party each candidate belongs to
 cand_data <- read.csv('data/2016_candidates.csv')
@@ -24,90 +29,109 @@ state_info <- read.csv('data/state_info.csv')
 # Read in data mapping zip codes to lat/lng coordinates
 zip_data <- read.csv('data/location_data.csv', stringsAsFactors = FALSE)
 
-# Reshape data for analysis
-clean <- function(file_name) {
+# read in raw data
+data <- read.csv(file_name, stringsAsFactors = FALSE)
+
+
+###############################
+## Reshape data for analysis ##
+###############################
+
+# pick useful columns
+formatted_data <- data %>%
+   select(
+      cand_nm,
+      recipient_nm,
+      disb_amt,
+      disb_dt,
+      recipient_city,
+      recipient_st,
+      recipient_zip,
+      disb_desc
+   ) %>%
    
-   # read in raw data
-   data <- read.csv(file_name, stringsAsFactors = FALSE)
+   # give columns better names
+   rename(
+      "Candidate" = cand_nm,
+      "Recipient" = recipient_nm,
+      "Amount" = disb_amt,
+      "Date" = disb_dt,
+      "City" = recipient_city,
+      "State" = recipient_st,
+      "Zip" = recipient_zip,
+      "Category" = disb_desc
+   ) %>%
    
-   # pick useful columns
-   formatted_data <- data %>%
-      select(
-         cand_nm,
-         recipient_nm,
-         disb_amt,
-         disb_dt,
-         recipient_city,
-         recipient_st,
-         recipient_zip,
-         disb_desc
-      ) %>%
-      
-      # give columns better names
-      rename(
-         "Candidate" = cand_nm,
-         "Recipient" = recipient_nm,
-         "Amount" = disb_amt,
-         "Date" = disb_dt,
-         "City" = recipient_city,
-         "State" = recipient_st,
-         "Zip" = recipient_zip,
-         "Category" = disb_desc
-      ) %>%
-      
-      # remove entries outside of the US
-      filter(State %in% state_info$abbreviation) %>% 
-      
-      # add candidate data
-      left_join(cand_data, by = "Candidate") %>%
-      
-      # change format of data
-      mutate(
-         Date = as.Date(Date, "%d-%b-%y"),
-         City = str_to_title(City),
-         Candidate = Popular_name,
-         Zip = str_sub(Zip, 1, 5),
-         Zip = as.numeric(Zip)
-      ) %>% 
-      
-      # fix extraneous data
-      # (WARNING: May not generalize to all datasets)
-      filter(City != "Berlin Germany") %>% 
-      mutate(Zip = ifelse(Zip == 0, 20001, Zip),
-             Zip = ifelse(City == "Sydney", 36117, Zip),
-             Zip = ifelse(Recipient == "SP PLUS CORPORATION", 60601, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "JIMMY JOHN'S", 29201, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "TAXI-PASS", 10001, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "SERESC CONFERENCE CENTER", 3110, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "EMBASSY SUITES", 22101, Zip),
-             Zip = ifelse(is.na(Zip) & Candidate == "James Webb" & City == "San Diego", 92106, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "WYNDHAM", 97201, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "HILTON NEWARK PENN STATION", 7102, Zip),
-             Zip = ifelse(is.na(Zip) & Recipient == "HOLIDAY INN EXPRESS", 3101, Zip),
-             Zip = ifelse(is.na(Zip) & City == "Chicago", 60612, Zip),
-             
-             City = ifelse(City == "" & Candidate == "Chris Christie" & State == "NH", "Manchester", City)
-             ) %>%
-      
-      # add lat/lng
-      left_join(select(zip_data, 
-                       Zip, 
-                       Lat, 
-                       Lng), 
-                by = "Zip") %>%
-      
-      # rearrange columns
-      select(1, 10:11, 2:8, 12:13) %>% 
-      
-      # sort rows
-      arrange(desc(Running), Candidate, Date, Amount, State, Category)
+   # remove entries outside of the US
+   filter(State %in% state_info$abbreviation) %>% 
    
+   # add candidate data
+   left_join(cand_data, by = "Candidate") %>%
    
+   # change format of data
+   mutate(
+      Date = as.Date(Date, "%d-%b-%y"),
+      City = str_to_title(City),
+      Candidate = Popular_name,
+      Zip = str_sub(Zip, 1, 5),
+      Zip = as.numeric(Zip)
+   ) %>% 
    
-   # set output filename to be original filename with "-clean" appended
-   new_file_name <- paste0(file_path_sans_ext(file_name), "-clean.csv")
+   # fix extraneous data
+   # (WARNING: May not generalize to all datasets)
+   filter(City != "Berlin Germany") %>% 
+   mutate(Zip = ifelse(Zip == 0, 20001, Zip),
+          Zip = ifelse(City == "Sydney", 36117, Zip),
+          Zip = ifelse(Recipient == "SP PLUS CORPORATION", 60601, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "JIMMY JOHN'S", 29201, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "TAXI-PASS", 10001, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "SERESC CONFERENCE CENTER", 3110, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "EMBASSY SUITES", 22101, Zip),
+          Zip = ifelse(is.na(Zip) & Candidate == "James Webb" & City == "San Diego", 92106, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "WYNDHAM", 97201, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "HILTON NEWARK PENN STATION", 7102, Zip),
+          Zip = ifelse(is.na(Zip) & Recipient == "HOLIDAY INN EXPRESS", 3101, Zip),
+          Zip = ifelse(is.na(Zip) & City == "Chicago", 60612, Zip),
+          
+          City = ifelse(City == "" & Candidate == "Chris Christie" & State == "NH", "Manchester", City)
+          ) %>%
    
-   # write new file
-   return(write.csv(formatted_data, file = new_file_name, row.names = FALSE))
+   # rearrange columns
+   select(1, 10:11, 2:8) %>%
    
-}
+   # add lat/lng
+   left_join(select(zip_data, 
+                    Zip, 
+                    Lat, 
+                    Lng), 
+             by = "Zip") %>%
+   
+   # sort rows
+   arrange(desc(Running), Candidate, Date, Amount, State, Category)
+
+
+#########################
+## Output cleaned file ##
+#########################
+
+# set output filename to be original filename with "-clean" appended
+new_file_name <- paste0(file_path_sans_ext(file_name), "-clean.csv")
+
+# write new file
+write.csv(formatted_data, file = new_file_name, row.names = FALSE)
+   
+
+# #################
+# ## Bug testing ##
+# #################
+# 
+# pre_latlng <- formatted_data
+# 
+# bad_zip_df <- filter(formatted_data, is.na(Lat) | is.na(Lng)) %>% 
+#    select(1, 4, 6:9, 11:12)
+# 
+# bad_zip_v <- unique(bad_zip_df$Zip)
+# 
+# zip_source <- unique(zip_data$Zip)
+
+
