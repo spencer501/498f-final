e632115d96055c4287752173807111c36c398f7c
fionc <fionc@uw.edu>
2016-03-11 01:52:56 -0800

    made intro blurb

--- ui.R
+++ ui.R
@@ -17,7 +17,26 @@ shinyUI(
       strong("2016 Presidential Campaign Spending"),
    
       # Intro tab
-      tabPanel("Intro"),
+      tabPanel("Intro",
+               h1("Project Description"),
+               h3("Team Members: Fion, Jared, and Spencer"),
+               br(),
+               p("For our final project for INFO 498F, we are looking at the 2016 Presidential 
+                 Campaign Finance Expenditure Data. This dataset was collected by the Federal
+                 Election Commission (FEC), which is an independent regulatory agency that 
+                 discloses campaign finance information, contributions, and public funding for
+                 Presidential elections. This dataset includes the name of the candidates running,
+                 the amount of expenses, the date and location (city, state, zip, longitude, and 
+                 latitude) of the transactions, and the category of expenses. Additionally, more 
+                 information about the dataset can be found via this link: 
+                 http://www.fec.gov/disclosurep/pnational.do."),
+               br(),
+               p("In our project, we will be summarizing financial information disclosed by presidential
+                 candidates who has reported at least $100,000 in contributions from individuals other 
+                 than the candidate. Our main target audience is the general public who are interested in 
+                 presidential campaign finances. However, other audiences may include other candidates, 
+                 different interest groups, voters, donors, etc. who are interested in the expenditures 
+                 of the candidates. ")),
    
       # Individual data
       tabPanel("Individual Information",
