59dc02496afd4967669f430fc2255b63897ff1ef
fionc <fionc@uw.edu>
2016-03-10 22:40:12 -0800

    pulling and rebasing

--- scripts/pie_chart.R
+++ scripts/pie_chart.R
@@ -1,17 +1,14 @@
-#########################################
-# `pie_chart.R`
-# This produces a pie chart that shows how much each 
-# candidate spent for the 2016 presidential campaign. 
-#########################################
 
 # Required packages
 library(plotly)
 library(dplyr)
 
-# Makes a pie chart that looks at the expenses
+
+# makes a summary plot of expenses
 tot_expense <- function (expense_data) {
   
-    plot_ly(expense_data,
+  
+  plot_ly(expense_data,
           labels = Candidate,
           values = total,
           type = "pie") %>%
